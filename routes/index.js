35282a0f54e1a45a86edb4acd7aeefeb11b63134
kevinmaiden7 <kevin.m.gallego@gmail.com>
2021-01-23 10:36:36 -0500

    Setting up api

---
+++ routes/index.js
@@ -0,0 +1,38 @@
+var express = require('express');
+var router = express.Router();
+var breaker = require('../breaker');
+
+// GET
+router.get('/index', function(req, res, next) {
+    res.json({
+      response: '200 OK'
+    });
+});
+
+// GET
+router.get('/play/:number', function(req, res, next) {
+  n = [];
+  numbers = req.params.number;
+
+  for (var i = 0, len = numbers.length; i < len; i += 1) {
+    n.push(+numbers.charAt(i));
+  }
+
+  result = breaker.evaluate(n[0], n[1], n[2], n[3]);
+  res.json({
+    result
+  });
+});
+
+// POST
+router.post('/setup', function (req, res, next) {
+  console.log("POST");
+  let data = req.body;
+  console.log(data);
+  breaker.set(data.a, data.b, data.c, data.d);
+  res.json({
+    response: '200 OK'
+  });
+});
+
+module.exports = router;
