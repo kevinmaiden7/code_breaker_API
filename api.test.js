f1602b864a21f094458688fba20c4cd049d34a95
kevinmaiden7 <kevin.m.gallego@gmail.com>
2021-01-29 19:26:14 -0500

    Adjusting api test

--- api.test.js
+++ api.test.js
@@ -4,16 +4,16 @@ const app = require("./app");
 describe("testing /setup path", () => {
     test("it should return status code 200", done => {
         request(app)
-        .get("/setup")
+        .get("/setup/1234")
         .then(response => {
-            expect(response.statusCode).toBe(404);
+            expect(response.statusCode).toBe(200);
             done();
         });
     });
 
-    test("it should return application/json content-type", done => {
+    test("it should return text/html content-type", done => {
         request(app)
-        .get("/setup")
+        .get("/setup/1234")
         .then(response => {
             expect(response.type).toBe("text/html");
             done();
